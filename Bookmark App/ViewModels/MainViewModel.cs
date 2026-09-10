b0a2445b39bfdff4ae7a90a87d831d12793eca3a
Dylano de Vries <dylanoyaron@gmail.com>
2026-09-10 11:44:42 +0200

    refactor: simplify image controls and remove URL fetching

--- Bookmark App/ViewModels/MainViewModel.cs
+++ Bookmark App/ViewModels/MainViewModel.cs
@@ -289,7 +289,6 @@ namespace Bookmark_App.ViewModels
                 listVM.LoadItems(listVM._list, listVM.SelectedGenreSortOption, listVM.SelectedSortingOption, listVM.FilteringTitle, listVM.Status, listVM.ItemsPerPage, (int)listVM.CurrentPage);
             }
             ListItemDetailViewModel.ResetGenres();
-            ListItemDetailViewModel.ImageLink = string.Empty;
         }
         private void OpenEditList(Models.List currentList)
         {
